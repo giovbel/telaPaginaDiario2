419238056719c68042c7e13976f000ea8512dd61
GIOVANNA SILVA BELO <23111786@dsn127m1249387.cfp127aluno.local>
2024-10-08 16:17:21 -0300

    sei la

---
+++ app/src/main/java/br/senai/jandira/sp/telacriarpostdiario/MainActivity.kt
@@ -0,0 +1,246 @@
+package br.senai.jandira.sp.telacriarpostdiario
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.jandira.sp.telacriarpostdiario.ui.theme.TelaCriarPostDiarioTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TelaCriarPostDiarioTheme {
+                telacriar()
+                }
+            }
+        }
+    }
+
+@Composable
+fun telacriar() {
+
+  var  (title, setTitle) = remember {
+      mutableStateOf("")
+  }
+
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFE4EFFF))
+    ) {
+        Box(
+            modifier = Modifier
+                .height(70.dp)
+                .fillMaxWidth()
+                .background(
+                    Brush.horizontalGradient(
+                        colors = listOf(
+                            Color(0xFF213787),
+                            Color(0xFF245FB0),
+                            Color(0xFF6E96E8)
+                        )
+                    ),
+                )
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .height(60.dp)
+                        .width(60.dp)
+                )
+                Text(
+                    text = "CRIAR",
+                    fontSize = 20.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Medium
+                )
+                Card(
+                    modifier = Modifier
+                        .size(50.dp)
+                        .border(4.dp, Color(0xFF9DBFEF), RoundedCornerShape(30.dp)),
+                    shape = RoundedCornerShape(100.dp),
+                ) {
+
+                }
+            }
+
+        }
+        Box(
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ){
+            Card(
+                modifier = Modifier
+                    .width(350.dp)
+                    .height(570.dp),
+                colors = CardDefaults.cardColors(Color(0xFFC1DBFF)),
+            ) {
+                Card {
+                    TextField(
+                        value = title,
+                        onValueChange = { setTitle(it) },
+                        label = { Text("Título") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            }
+
+        }
+
+        Box(
+            modifier = Modifier
+                .height(70.dp)
+                .fillMaxWidth()
+                .background(
+                    Brush.horizontalGradient(
+                        colors = listOf(
+                            Color(0xFF213787),
+                            Color(0xFF245FB0),
+                            Color(0xFF6E96E8)
+                        )
+                    ),
+                )
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Button(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .size(72.dp)
+                        .background(Color.Transparent),
+                    colors = ButtonDefaults.buttonColors(Color.Transparent)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.noteicon),
+                        contentDescription = "",
+                        modifier = Modifier.size(60.dp)
+                    )
+
+                }
+                Button(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .size(72.dp)
+                        .background(Color.Transparent),
+                    colors = ButtonDefaults.buttonColors(Color.Transparent)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.chaticon),
+                        contentDescription = "",
+                        modifier = Modifier.size(60.dp)
+                    )
+
+                }
+                Button(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .size(72.dp)
+                        .background(Color.Transparent),
+                    colors = ButtonDefaults.buttonColors(Color.Transparent)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.diaryicon),
+                        contentDescription = "",
+                        modifier = Modifier.size(60.dp)
+                    )
+
+                }
+                Button(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .size(72.dp)
+                        .background(Color.Transparent),
+                    colors = ButtonDefaults.buttonColors(Color.Transparent)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.autoajudaicon),
+                        contentDescription = "",
+                        modifier = Modifier.size(60.dp)
+                    )
+
+                }
+                Button(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .size(72.dp)
+                        .background(Color.Transparent),
+                    colors = ButtonDefaults.buttonColors(Color.Transparent)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.doacaoicon),
+                        contentDescription = "",
+                        modifier = Modifier.size(60.dp)
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    TelaCriarPostDiarioTheme {
+        telacriar()
+    }
+}
